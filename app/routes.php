ca12bb0e11866ed8c61307583abc5b40abea68d5
murum <cr222cs@student.lnu.se>
2014-06-07 14:24:10 +0200

    update $_ENV to getenv

--- app/routes.php
+++ app/routes.php
@@ -3,10 +3,10 @@
 Route::get('/', function()
 {
     return [
-        'HOST' => $_ENV['DB_HOST'],
-        'NAME' => $_ENV['DB_NAME'],
-        'USERNAME' => $_ENV['DB_USERNAME'],
-        'PASSWORD' => $_ENV['DB_PASSWORD'],
+        'HOST' => getenv('DB_HOST'),
+        'NAME' => getenv('DB_NAME'),
+        'USERNAME' => getenv('DB_USERNAME'),
+        'PASSWORD' => getenv('DB_PASSWORD'),
         'ENVIRONMENT' => App::environment()
     ];
 });
