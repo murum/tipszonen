075cd8b7a7a6c660ddd48694ced1f2c89cc08d74
murum <cr222cs@student.lnu.se>
2014-07-07 21:25:54 +0200

    update all coupons to correct

--- app/views/admin/coupons.blade.php
+++ app/views/admin/coupons.blade.php
@@ -3,12 +3,20 @@
 @section('content')
 <h1>Rätta kuponger</h1>
 <h2>Pågående omgångar</h2>
-    <ul></ul>
-    @foreach(CouponDetail::ongoingCoupons() as $coupon)
-        <li>
-            {{ link_to_route('admin.liverattning.get_single', $coupon->game_week.' - '.$coupon->product->name, ['id' => $coupon->id]) }}
-        </li>
-    @endforeach
+    <ul class="list-group">
+        @foreach(CouponDetail::ongoingCoupons() as $coupon)
+            <li class="list-group-item">
+                {{
+                    link_to_route(
+                        'admin.liverattning.get_single',
+                        $coupon->game_week.' - '.$coupon->product->name,
+                        ['id' => $coupon->id],
+                        ['class' => 'btn btn-disabled btn-lg btn-success btn-'.$coupon->product->slug]
+                    )
+                }}
+            </li>
+        @endforeach
+    </ul>
 <h2>Kommande omgångar</h2>
     <ul class="list-group">
         @foreach(CouponDetail::comingCoupons() as $coupon)
@@ -25,9 +33,18 @@
         @endforeach
     </ul>
 <h2>Avslutade omgångar</h2>
-    @foreach(CouponDetail::endedCoupons() as $coupon)
-        <li>
-            {{ link_to_route('admin.liverattning.get_single', $coupon->game_week.' - '.$coupon->product->name, ['id' => $coupon->id]) }}
-        </li>
-    @endforeach
+    <ul class="list-group">
+        @foreach(CouponDetail::endedCoupons() as $coupon)
+            <li class="list-group-item">
+                {{
+                    link_to_route(
+                        'admin.liverattning.get_single',
+                        $coupon->game_week.' - '.$coupon->product->name,
+                        ['id' => $coupon->id],
+                        ['class' => 'btn btn-disabled btn-lg btn-success btn-'.$coupon->product->slug]
+                    )
+                }}
+            </li>
+        @endforeach
+    </ul>
 @stop
