4775863822eae82dc4c905261309efdc12becc04
murum <cr222cs@student.lnu.se>
2014-09-20 08:41:23 +0200

    add åäö to coupon name

--- app/validators.php
+++ app/validators.php
@@ -10,5 +10,5 @@ Validator::extend('alpha_spaces', function($attribute, $value)
 
 Validator::extend('alpha_num_spaces', function($attribute, $value)
 {
-    return preg_match('/^([-a-z0-9_-\s])+$/i', $value);
+    return preg_match('/^([-a-zåäöA-ZÅÄÖ0-9_-\s])+$/i', $value);
 });
